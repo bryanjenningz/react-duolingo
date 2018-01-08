46607964e82a291a263f912201bf75cae421a947
Bryan Jennings <bryanjenningz@gmail.com>
2018-01-07 20:09:33 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ maxWidth: 500, margin: "0 auto" }}>
+        <div style={{ textAlign: "center" }}>Hello</div>
+      </div>
+    );
+  }
+}
+
+export default App;
