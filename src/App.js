f576d5909eaf26d12b2f3c6a060933e43610295b
Bryan Jennings <bryanjenningz@gmail.com>
2018-01-07 20:41:22 -0800

    add bottom button

--- src/App.js
+++ src/App.js
@@ -35,12 +35,60 @@ const ProgressBar = ({ percent }) => (
   </div>
 );
 
+const SelectedBlocks = ({ blocks }) => (
+  <div style={{ display: "flex", flexWrap: "wrap" }}>
+    {blocks.map(block => (
+      <div key={block.id} style={{ background: "white", padding: 5 }}>
+        {block.text}
+      </div>
+    ))}
+  </div>
+);
+
+const UnselectedBlocks = ({ blocks }) => (
+  <div style={{ display: "flex", flexWrap: "wrap" }}>
+    {blocks.map(block => (
+      <div key={block.id} style={{ background: "white", padding: 5 }}>
+        {block.text}
+      </div>
+    ))}
+  </div>
+);
+
+const CheckButton = () => (
+  <div
+    style={{
+      width: "100%",
+      color: "#888",
+      background: "#ccc",
+      height: 50,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 100
+    }}
+  >
+    CHECK
+  </div>
+);
+
 class App extends Component {
+  state = { blocks: [], selectedBlockIds: [] };
+
   render() {
+    const { blocks, selectedBlockIds } = this.state;
+    const selectedBlocks = blocks.filter(block =>
+      selectedBlockIds.includes(block.id)
+    );
+    const unselectedBlocks = blocks.filter(
+      block => !selectedBlockIds.includes(block.id)
+    );
     return (
       <div style={{ maxWidth: 500, margin: "0 auto", padding: "0 1em" }}>
         <ProgressBar percent={50} />
-        <div style={{ textAlign: "center" }}>Hello</div>
+        <SelectedBlocks blocks={selectedBlocks} />
+        <UnselectedBlocks blocks={unselectedBlocks} />
+        <CheckButton />
       </div>
     );
   }
