7fe01a29f3e78b40fc6065848397b5b6e63187c3
Bryan Jennings <bryanjenningz@gmail.com>
2018-01-09 18:01:58 -0800

    add Learn.js file

---
+++ src/Learn.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Learn extends Component {
+  render() {
+    return (
+      <div>
+        Learn
+      </div>
+    );
+  }
+}
+
+export default Learn;
